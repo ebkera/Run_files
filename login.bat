d175daec40c77925d22c64100c777b93a9caddf6
ebkera <ebk_era@hotmail.com>
2020-02-08 13:51:13 -0600

    Login and session scripts

---
+++ login.bat
@@ -0,0 +1,4 @@
+@echo off
+title Logging into Argonne - Mega
+echo Logging into Argonne...
+ssh -v -L 33301:carbon:22  rathnayake@mega.cnm.anl.gov
---
+++ session.bat
@@ -0,0 +1,5 @@
+@echo off
+title Logging into Carbon session on Argonne
+echo Loging into Carbon session on Argonne...
+ssh -Y -p 33301 rathnayake@localhost
+pause
